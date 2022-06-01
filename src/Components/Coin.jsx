62832cedf2980b479fc9f9f0a208e1efde6e3583
sebastian <sandlerzop@gmail.com>
2022-06-01 12:28:28 -0500

    styles added to coin component

--- src/Components/Coin.jsx
+++ src/Components/Coin.jsx
@@ -1,33 +1,36 @@
+import { arrow } from '../Assets/Images/Util/index'
+
 export default function Coin({ data }) {
-  console.log(data)
   const { name, symbol, icon, price, priceChange1d, priceChange1w } = data
   return (
-    <div>
-      <div>
-        <div>
+    <div className="coin">
+      <div className="coin__top">
+        <div className="coin__top__img">
           <img src={icon} alt="" />
         </div>
-        <div>
-          <span>
-            {name} <span>{symbol}</span>
+        <div className="coin__top__title">
+          <span className="coin__top__title__name">
+            {name} <span>({symbol})</span>
           </span>
-          <span>$ {price}</span>
+          <span>$ {price.toLocaleString()}</span>
         </div>
-        <div>save</div>
+        <div className="coin__top__save"></div>
       </div>
-      <div>
+      <div className="coin__bottom">
         <span>24h %</span>
-        <div>
-          <img src={''} alt="" />
+        <div className="coin__bottom__priceChange">
+          <div className="coin__bottom__priceChange__img green__img">
+            <img src={arrow} alt="" />
+          </div>
+          <span className="green">{priceChange1d} %</span>
         </div>
-        <span>{priceChange1d}</span>
-      </div>
-      <div>
         <span>7d %</span>
-        <div>
-          <img src={''} alt="" />
+        <div className="coin__bottom__priceChange">
+          <div className="coin__bottom__priceChange__img red__img">
+            <img src={arrow} alt="" />
+          </div>
+          <span className="red">{priceChange1w} %</span>
         </div>
-        <span>{priceChange1w}</span>
       </div>
     </div>
   )
