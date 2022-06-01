a0999435290287b15c275b16ad5c827e668e7a0e
sebastian <sandlerzop@gmail.com>
2022-06-01 08:05:22 -0500

    updated routes, navbar route added and index route

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,15 +1,18 @@
 import { Route, Routes } from 'react-router-dom'
 import { Main, High, Low, Save, News, Error } from '../Pages/index'
+import Navbar from '../Components/Navbar'
 
 export default function App() {
   return (
     <Routes>
-      <Route to="/" element={Main} />
-      <Route to="/high" element={High} />
-      <Route to="/low" element={Low} />
-      <Route to="/save" element={Save} />
-      <Route to="/news" element={News} />
-      <Route to="*" element={Error} />
+      <Route to="/" element={<Navbar />}>
+        <Route index element={<Main />} />
+        <Route to="/high" element={<High />} />
+        <Route to="/low" element={<Low />} />
+        <Route to="/save" element={<Save />} />
+        <Route to="/news" element={<News />} />
+        <Route to="*" element={<Error />} />
+      </Route>
     </Routes>
   )
 }
