0f28bc86af09051d393b0acda6a28b2cd809e6c5
sebastian <sandlerzop@gmail.com>
2022-05-31 19:52:54 -0500

    feching data

---
+++ src/Util/Data.js
@@ -0,0 +1,29 @@
+const API_URL = 'https://api.coinstats.app/public/v1'
+
+export const getCoins = async (currency = 'USD', limit = '0') => {
+  try {
+    const response = await fetch(
+      `${API_URL}coins?skip=0&limit=${limit}&currency=${currency}`
+    )
+    if (response.ok) {
+      const data = await response.json()
+      return data
+    }
+    throw new Error('Get coins fail!')
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const getNews = async (limit = 20) => {
+  try {
+    const response = await fetch(`${API_URL}news?skip=0&limit=${limit}`)
+    if (response.ok) {
+      const data = await response.json()
+      return data
+    }
+    throw new Error('Get news fail!')
+  } catch (error) {
+    console.log(error)
+  }
+}
