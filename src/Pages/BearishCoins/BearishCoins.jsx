0bea01e0ea622de4a8980c6c43f138335ebfdc86
sebastian <sandlerzop@gmail.com>
2022-06-02 19:20:11 -0500

    bearish component created

--- src/Pages/BearishCoins/BearishCoins.jsx
+++ src/Pages/BearishCoins/BearishCoins.jsx
@@ -1,3 +1,16 @@
+import { useSelector } from 'react-redux'
+import { selectBearishCoins } from '../Main/MainSlice'
+import Coin from '../../Components/Coin'
+
 export default function BearishCoins() {
-  return <div>BearishCoins</div>
+  const bullishCoins = useSelector(selectBearishCoins)
+  const mapBullishCoins = bullishCoins.map(data => (
+    <Coin data={data} key={data.id} />
+  ))
+  return (
+    <main className="bullish-coins">
+      <h1>Bearish Coins</h1>
+      <section className="bullish-coins__coins">{mapBullishCoins}</section>
+    </main>
+  )
 }
