f5df3e0b0898baa9f74d54954d8101dcb480fb52
sebastian <sandlerzop@gmail.com>
2022-06-13 14:44:46 -0500

    current btc added to news

--- src/Pages/News/News.jsx
+++ src/Pages/News/News.jsx
@@ -1,6 +1,7 @@
 import { selectNews } from './NewsSlice'
 import { useSelector } from 'react-redux'
 import New from '../../Components/New'
+import CurrentBTC from '../../Components/CurrentBTC'
 
 export default function News() {
   const news = useSelector(selectNews)
@@ -8,7 +9,10 @@ export default function News() {
 
   return (
     <main className="simply-page">
-      <h1>News</h1>
+      <div className="simply-page__top">
+        <h1>News</h1>
+        <CurrentBTC />
+      </div>
       <div className="simply-page__container simply-page__container--news">
         {mapNews}
       </div>
