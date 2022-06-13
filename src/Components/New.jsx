774c716d6e70f220897f524e27b9b8a0ce268b27
sebastian <sandlerzop@gmail.com>
2022-06-12 21:15:46 -0500

    news done

--- src/Components/New.jsx
+++ src/Components/New.jsx
@@ -1,12 +1,12 @@
-import { randomImg } from '../Util/Util'
+import { randomImg, timeDifference } from '../Util/Util'
 import { news1, news2, news3, news4 } from '../Assets/Images/NewsImages'
 
 export default function New({ data }) {
-  const { description, feedDate, imgURL, link, source, title } = data
+  const { feedDate, imgURL, link, source, title } = data
   const arrImg = [news1, news2, news3, news4]
 
   return (
-    <div className="new">
+    <a href={link} target="_blank" rel="noreferrer" className="new">
       <div className="new__img">
         <img
           src={imgURL}
@@ -17,7 +17,13 @@ export default function New({ data }) {
           }}
         />
       </div>
-      <p className="new__title">{title}</p>
-    </div>
+      <div className="new__body">
+        <p className="new__body__title">{title}</p>
+        <div className="new__body__source">
+          <span>Source: {source}</span>
+          <span>{timeDifference(feedDate)} ago</span>
+        </div>
+      </div>
+    </a>
   )
 }
