2c759f9bb2be805eb5807ea32f2364765f1fa673
sebastian <sandlerzop@gmail.com>
2022-06-13 14:26:16 -0500

    component added to the main page

--- src/Components/CurrentBTC.jsx
+++ src/Components/CurrentBTC.jsx
@@ -1,11 +1,13 @@
 import { useSelector } from 'react-redux'
 import { selectCoins } from '../Pages/Main/MainSlice'
+import { cutDecimals } from '../Util/Util'
+
 export default function CurrentBTC() {
   const [BTC] = useSelector(selectCoins)
 
   return (
     <div>
-      BTC = <span>{BTC.price}</span> USD
+      BTC = <span>{cutDecimals(BTC.price)}</span> USD
     </div>
   )
 }
